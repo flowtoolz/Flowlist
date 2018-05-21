ca6a50a388af6f5cbcdeaafc0046c97935b0fa54
Sebastian <contact@flowtoolz.com>
2018-05-21 14:47:42 +0200

    move extensions of SelectableTaskList to dedicated file

---
+++ Code/SelectableTaskList+Extensions.swift
@@ -0,0 +1,23 @@
+extension SelectableTaskList
+{
+    func debug()
+    {
+        print("════════════════════════════════════════════════\n\n" + description + "\n")
+    }
+    
+    var description: String
+    {
+        var desc = title.latestUpdate + ":"
+        
+        for i in 0 ..< numberOfTasks
+        {
+            guard let task = task(at: i) else { continue }
+            
+            desc += "\n- \(task.title.value ?? "untitled")"
+        }
+        
+        desc += "\n\nselection: \(selection.description)"
+        
+        return desc
+    }
+}
--- Code/SelectableTaskList.swift
+++ Code/SelectableTaskList.swift
@@ -1,30 +1,6 @@
 import SwiftObserver
 import SwiftyToolz
 
-extension SelectableTaskList
-{
-    func debug()
-    {
-        print("════════════════════════════════════════════════\n\n" + description + "\n")
-    }
-    
-    var description: String
-    {
-        var desc = title.latestUpdate + ":"
-        
-        for i in 0 ..< numberOfTasks
-        {
-            guard let task = task(at: i) else { continue }
-            
-            desc += "\n- \(task.title.value ?? "untitled")"
-        }
-        
-        desc += "\n\nselection: \(selection.description)"
-        
-        return desc
-    }
-}
-
 class SelectableTaskList: TaskList
 {
     // MARK: - Selection Dependent Editing
