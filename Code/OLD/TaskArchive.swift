2c306a4ee44caa409ce440283a0bd30e5f52021c
Sebastian <contact@flowtoolz.com>
2018-05-29 22:36:39 +0200

    implement file format migration and remove unused legacy code

--- Code/OLD/TaskArchive.swift
+++ Code/OLD/TaskArchive.swift
@@ -1,64 +1,50 @@
-//
-//  TaskArchive.swift
-//  TodayList
-//
-//  Created by Sebastian on 14/06/17.
-//  Copyright © 2017 Flowtoolz. All rights reserved.
-//
-
 import Foundation
+import SwiftObserver
 
 class TaskArchive: NSObject, NSCoding
 {
     required convenience init?(coder aDecoder: NSCoder)
     {
-        guard let uuid = aDecoder.decodeObject(forKey: "uuid") as? String else
+        guard let decodedUuid = aDecoder.decodeObject(forKey: "uuid") as? String else
         {
             Swift.print("Error decoding Task: could not decode UUID string")
             return nil
         }
         
-        let title = aDecoder.decodeObject(forKey: "title") as? String
-        let state = Task.State(rawValue: aDecoder.decodeInteger(forKey: "state"))
+        let decodedTask = Task()
         
-        self.init(with: Task(with: uuid, title: title, state: state))
+        decodedTask.title <- aDecoder.decodeObject(forKey: "title") as? String
         
+        if let state = LegacyState(rawValue: aDecoder.decodeInteger(forKey: "state"))
+        {
+            switch state
+            {
+            case .done: decodedTask.state <- .done
+            default: break
+            }
+        }
+        
+        self.init(with: decodedTask, uuid: decodedUuid)
+
         elementUuidsForDecoding = aDecoder.decodeObject(forKey: "elementUuids") as? [String]
     }
     
     var elementUuidsForDecoding: [String]?
     
-    func encode(with aCoder: NSCoder)
-    {
-        aCoder.encode(task.uuid, forKey: "uuid")
-        
-        if let title = task.title
-        {
-            aCoder.encode(title, forKey: "title")
-        }
-        
-        if let stateInteger = task.state?.rawValue
-        {
-            aCoder.encode(stateInteger, forKey: "state")
-        }
-        
-        if task.subtasks.count > 0
-        {
-            var subtaskUuids = [String]()
-            
-            for subtask in task.subtasks
-            {
-                subtaskUuids.append(subtask.uuid)
-            }
-            
-            aCoder.encode(subtaskUuids, forKey: "elementUuids")
-        }
-    }
+    func encode(with aCoder: NSCoder) { fatalError() }
     
-    init(with task: Task)
+    init(with task: Task, uuid: String)
     {
         self.task = task
+        self.uuid = uuid
     }
     
     let task: Task
+    let uuid: String
+}
+
+enum LegacyState: Int
+{
+    // state == nil is default and kind of a backlog or "no specific state"
+    case inProgress, onHold, done, archived
 }
--- Code/Store+Persistence.swift
+++ Code/Store+Persistence.swift
@@ -15,6 +15,14 @@ extension Store
     
     func load()
     {
+        if FileManager.default.fileExists(atPath: legacyFilePath)
+        {
+            loadLegacyFile()
+            save()
+            try? FileManager.default.removeItem(atPath: legacyFilePath)
+            return
+        }
+        
         guard let loadedRoot = Task(from: fileUrl) else
         {
             log(error: "Failed to load tasks from " + fileUrl.absoluteString)
@@ -29,5 +37,74 @@ extension Store
     private var fileUrl: URL
     {
         return URL(fileURLWithPath: Bundle.main.bundlePath + "/flowlist.json")
+    }
+    
+    // MARK: - Legacy
+    
+    func loadLegacyFile()
+    {
+        // read task archives
+        guard let archive = NSKeyedUnarchiver.unarchiveObject(withFile: legacyFilePath) as? [TaskArchive]
+        else
+        {
+            return
+        }
+        
+        // create task hash map
+        var tasksByUuid = [String: Task]()
+        
+        for archiveTask in archive
+        {
+            tasksByUuid[archiveTask.uuid] = archiveTask.task
+        }
+        
+        // connect task hierarchy
+        for archiveTask in archive
+        {
+            let task = archiveTask.task
+            
+            if let elementUuids = archiveTask.elementUuidsForDecoding
+            {
+                for elementUuid in elementUuids
+                {
+                    if let element = tasksByUuid[elementUuid]
+                    {
+                        task.branches.append(element)
+                        
+                        element.root = task
+                    }
+                }
+            }
+        }
+        
+        // find root and reset
+        var potentialUnarchivedRoot: Task?
+        
+        for task in tasksByUuid.values
+        {
+            if task.root == nil
+            {
+                if potentialUnarchivedRoot != nil
+                {
+                    print("Error: found multiple root containers in unarchived tasks")
+                    return
+                }
+                
+                potentialUnarchivedRoot = task
+            }
+        }
+        
+        guard let unarchivedRoot = potentialUnarchivedRoot else
+        {
+            print("Error: found no root container in unarchived tasks")
+            return
+        }
+        
+        root = unarchivedRoot
+    }
+    
+    private var legacyFilePath: String
+    {
+        return Bundle.main.bundlePath + "/UserData.plist"
     }
 }
