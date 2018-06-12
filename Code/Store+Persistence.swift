9fcea054603f547cefa8a2ba2fa7fffd39c94908
Sebastian <contact@flowtoolz.com>
2018-06-12 17:55:43 +0200

    store json file in user's (app sandbox's) document directory

--- Code/Store+Persistence.swift
+++ Code/Store+Persistence.swift
@@ -6,35 +6,34 @@ extension Store
 {
     func load()
     {
-        guard let loadedRoot = Task(from: fileUrl) else
+        guard let fileUrl = fileUrl, let loadedRoot = Task(from: fileUrl) else
         {
-            log(warning: "Failed to load tasks from " + fileUrl.absoluteString)
+            let fileString = self.fileUrl?.absoluteString ?? "file"
+            log(warning: "Failed to load tasks from " + fileString)
             save()
             return
         }
         
         loadedRoot.recoverRoots()
-        loadedRoot.title <- bundlePath.fileName
+        loadedRoot.title <- NSFullUserName()
         
         root = loadedRoot
     }
     
     func save()
     {
-        guard let _ = root.save(to: fileUrl) else
+        guard let fileUrl = fileUrl, let _ = root.save(to: fileUrl) else
         {
-            log(error: "Failed to save tasks to " + fileUrl.absoluteString)
+            let fileString = self.fileUrl?.absoluteString ?? "file"
+            log(error: "Failed to save tasks to " + fileString)
             return
         }
         
-        root.title <- bundlePath.fileName
+        root.title <- NSFullUserName()
     }
     
-    private var fileUrl: URL
+    private var fileUrl: URL?
     {
-        return URL(fileURLWithPath: bundlePath + "/" + jsonFileName)
+        return URL.documentDirectory?.appendingPathComponent("flowlist.json")
     }
-    
-    private var bundlePath: String { return Bundle.main.bundlePath }
-    private var jsonFileName: String { return "flowlist.json" }
 }
