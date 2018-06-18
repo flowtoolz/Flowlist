1ad0580f66967cba4d2b5a4b604adc1426e8e72b
Sebastian <contact@flowtoolz.com>
2018-06-18 14:40:39 +0200

    read product identifiers from plist file

--- Code/macOS/InAppPurchase.swift
+++ Code/macOS/InAppPurchase.swift
@@ -1,5 +1,31 @@
 import Foundation
 
+func printProductIdentifiers()
+{
+    let identifiers = loadProductIdentifiers()
+    
+    for id in identifiers
+    {
+        print(id)
+    }
+}
+
+func loadProductIdentifiers() -> [String]
+{
+    guard let fileUrl = productIdentifiersFileUrl,
+        let data = try? Data(contentsOf: fileUrl)
+    else
+    {
+        return []
+    }
+    
+    let plist = try? PropertyListSerialization.propertyList(from: data,
+                                                            options: [],
+                                                            format: nil)
+    
+    return (plist as? [String]) ?? []
+}
+
 var productIdentifiersFileUrl: URL?
 {
     return Bundle.main.url(forResource: "product_identifiers",
