f6ce0944acf633df308a3d3ab589eb3b99efeba3
Sebastian <contact@flowtoolz.com>
2018-06-20 15:49:09 +0200

    allow to check whether user reached task limit

--- Code/FullVersion.swift
+++ Code/FullVersion.swift
@@ -1,14 +1,27 @@
 import Foundation
 
+var reachedTaskNumberLimit: Bool
+{
+    return !isFullVersion && Task.numberOfTasks >= maxNumberOfTasksInTrial
+}
+
 var isFullVersion: Bool
 {
     get
     {
-        return UserDefaults.standard.string(forKey: userNameKey) != nil
+        if let fullVersion = isFullVersion_Cached { return fullVersion }
+        
+        let fullVersion = UserDefaults.standard.string(forKey: userNameKey) != nil
+        
+        isFullVersion_Cached = fullVersion
+        
+        return fullVersion
     }
     
     set
     {
+        isFullVersion_Cached = newValue
+        
         if newValue
         {
             UserDefaults.standard.set(NSFullUserName(), forKey: userNameKey)
@@ -20,4 +33,6 @@ var isFullVersion: Bool
     }
 }
 
+fileprivate var isFullVersion_Cached: Bool?
+fileprivate let maxNumberOfTasksInTrial = 100
 fileprivate let userNameKey = "UserName"
