88a89b459fd0308198c07a14069774be150ca71e
Sebastian <contact@flowtoolz.com>
2018-06-21 16:19:23 +0200

    clean up TaskState priority

--- Code/TaskState.swift
+++ Code/TaskState.swift
@@ -1,15 +1,11 @@
 enum TaskState: Int, Codable
 {
-    // FIXME: make "undone" an explicit state instead of using nil for it. then remove this function and just use rawValue
+    case inProgress = 0, done = 2, trashed = 3
     
-    case inProgress, done = 2, trashed = 3
+    // FIXME: make "undone" an explicit state (raw value 1) instead of using nil for it. then remove this function and just use rawValue
     
     static func priority(of state: TaskState?) -> Int
     {
-        guard let state = state else { return 1 }
-        
-        if state == .inProgress { return 0 }
-        
-        return state.rawValue + 1
+        return state?.rawValue ?? 1
     }
 }
