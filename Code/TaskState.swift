70b44d8ca8f9d7855a49ff461d162d26e0a9d855
Sebastian <contact@flowtoolz.com>
2018-06-21 11:31:06 +0200

    fix reading states from file: raw value 2 is and will be state "done"

--- Code/TaskState.swift
+++ Code/TaskState.swift
@@ -1,8 +1,9 @@
 enum TaskState: Int, Codable
 {
-    case inProgress, done, trashed
-    
     // FIXME: make "undone" an explicit state instead of using nil for it. then remove this function and just use rawValue
+    
+    case inProgress, done = 2, trashed = 3
+    
     static func priority(of state: TaskState?) -> Int
     {
         guard let state = state else { return 1 }
