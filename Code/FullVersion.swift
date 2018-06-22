90816b00788abc5cde5731f24b26bf42b10d6637
Sebastian <contact@flowtoolz.com>
2018-06-22 15:57:18 +0200

    treat BETA as full version, use DEBUG for developing in-app purchase

--- Code/FullVersion.swift
+++ Code/FullVersion.swift
@@ -2,17 +2,17 @@ import Foundation
 
 var reachedTaskNumberLimit: Bool
 {
-    #if RELEASE
     return !isFullVersion && Task.numberOfTasks >= maxNumberOfTasksInTrial
-    #else
-    return false
-    #endif
 }
 
 var isFullVersion: Bool
 {
     get
     {
+        #if BETA
+        return true
+        #endif
+        
         if let fullVersion = isFullVersion_Cached { return fullVersion }
         
         let fullVersion = UserDefaults.standard.string(forKey: userNameKey) != nil
