c1ac32cbcbc0582d9100b2215661e4ce11ac36ec
Sebastian <contact@flowtoolz.com>
2018-06-29 13:06:02 +0200

    fine tune item background colors

--- Code/Color+Style.swift
+++ Code/Color+Style.swift
@@ -2,13 +2,12 @@ import SwiftyToolz
 
 extension Color
 {
-    static let backlog = Color.gray(brightness: 0.95)
+    static let backlog = Color.gray(brightness: 0.97)
     static let background = Color.gray(brightness: 0.9)
-    static let done = Color.gray(brightness: 0.86)
+    static let done = Color.gray(brightness: 0.87)
     
     static let border = Color(0, 0, 0, 0.15)
     static let grayedOut = Color(0, 0, 0, 0.33)
-    
     static let flowlistBlue = Color(36, 145, 241)
     static let discountRed = Color(0.75, 0, 0, 0.75)
 }
