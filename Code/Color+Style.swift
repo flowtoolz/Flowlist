61fef1bb6b9b401d45cc9d265451a489b2024b75
Sebastian <contact@flowtoolz.com>
2018-06-29 19:02:46 +0200

    finalize colors

--- Code/Color+Style.swift
+++ Code/Color+Style.swift
@@ -3,11 +3,13 @@ import SwiftyToolz
 extension Color
 {
     static let backlog = Color.white
-    static let background = Color.gray(brightness: 0.94)
-    static let done = Color.gray(brightness: 0.88)
+    static let background = Color.gray(brightness: darkeningFactor)
+    static let done = Color.gray(brightness: powf(darkeningFactor, 2))
     
     static let border = Color(0, 0, 0, 0.15)
     static let grayedOut = Color(0, 0, 0, 0.33)
     static let flowlistBlue = Color(36, 145, 241)
     static let discountRed = Color(0.75, 0, 0, 0.75)
 }
+
+fileprivate let darkeningFactor: Float = 0.92
