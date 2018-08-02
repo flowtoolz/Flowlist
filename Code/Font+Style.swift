dee7d73dad8901df10aed9a1a1e84985bfe79324
Sebastian <contact@flowtoolz.com>
2018-08-02 12:05:56 +0200

    adjust list title font weight

--- Code/Font+Style.swift
+++ Code/Font+Style.swift
@@ -3,6 +3,6 @@ import SwiftyToolz
 extension Font
 {
     static let text = Font.system(size: 14, weight: .system)
-    static let listTitle = Font.system(size: 14, weight: .bold)
+    static let listTitle = Font.system(size: 14, weight: .semibold)
     static let title = Font.system(size: 26, weight: .bold)
 }
