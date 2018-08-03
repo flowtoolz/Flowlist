4a13d1e00f6b67807eb7289eddfc7a79335885f8
Sebastian <contact@flowtoolz.com>
2018-08-03 13:23:37 +0200

    make done items less dark

--- Code/Color+Style.swift
+++ Code/Color+Style.swift
@@ -4,7 +4,7 @@ extension Color
 {
     static let backlog = Color.white.with(alpha: 0.5)
     static let background = Color.white.with(alpha: 0.5)
-    static let done = Color.black.with(alpha: 0.075)
+    static let done = Color.black.with(alpha: 0.05)
     
     static let border = Color.white.with(alpha: 0.25)
     static let grayedOut = Color.black.with(alpha: 0.5)
