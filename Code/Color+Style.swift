c00ca243a0b05c2d2bdf90cf86b1997df041267e
Sebastian <contact@flowtoolz.com>
2018-09-08 09:47:51 +0200

    make selection stand out more from colored items

--- Code/Color+Style.swift
+++ Code/Color+Style.swift
@@ -6,7 +6,7 @@ extension Color
     static let background = Color.gray(brightness: 0.92)
     static let done = Color.gray(brightness: 0.92 * 0.92)
     static let windowBackground = Color.gray(brightness: 0.92 * 0.92)
-    static let selection = Color.gray(brightness: 0.4)
+    static let selection = Color.gray(brightness: 0.35)
     static let border = Color.black.with(alpha: 0.15)
     static let grayedOut = Color.black.with(alpha: 0.5)
     static let discountRed = Color(0.75, 0, 0, 0.75)
