55139be15d04a2a5932754eed69b5fe82392afa2
Sebastian <contact@flowtoolz.com>
2018-09-11 12:16:10 +0200

    rework SwiftConstrainer

--- Code/macOS/SwiftConstrainer.swift
+++ Code/macOS/SwiftConstrainer.swift
@@ -1,110 +1,243 @@
 import AppKit
 
-extension NSLayoutGuide: HasLayoutAnchors {}
-extension NSView: HasLayoutAnchors {}
+extension NSLayoutGuide: LayoutItem {}
+extension NSView: LayoutItem {}
 
 // MARK: -
 
-extension HasLayoutAnchors
+extension LayoutItem
 {
-    // MARK: Horizontal
+    // MARK: Position
     
     @discardableResult
-    func constrainLeft<LayoutObject: HasLayoutAnchors>(to target: LayoutObject,
-                                                       offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainCenter<Target: LayoutItem>(to target: Target) -> [NSLayoutConstraint]
+    {
+        return [constrainCenterX(to: target), constrainCenterY(to: target)]
+    }
+    
+    // MARK: Horizontal Position
+    
+    @discardableResult
+    func constrainLeft<Target: LayoutItem>(to target: Target,
+                                           offset: CGFloat = 0) -> NSLayoutConstraint
     {
         return constrain(.left, to: .left, of: target, offset: offset)
     }
     
     @discardableResult
-    func constrainLeft<LayoutObject: HasLayoutAnchors>(toRightOf target: LayoutObject,
-                                                       offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainLeft<Target: LayoutItem>(toRightOf target: Target,
+                                           offset: CGFloat = 0) -> NSLayoutConstraint
     {
         return constrain(.left, to: .right, of: target, offset: offset)
     }
     
     @discardableResult
-    func constrainRight<LayoutObject: HasLayoutAnchors>(to target: LayoutObject,
-                                                        offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainRight<Target: LayoutItem>(to target: Target,
+                                            offset: CGFloat = 0) -> NSLayoutConstraint
     {
         return constrain(.right, to: .right, of: target, offset: offset)
     }
     
     @discardableResult
-    func constrainRight<LayoutObject: HasLayoutAnchors>(toLeftOf target: LayoutObject,
-                                                        offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainRight<Target: LayoutItem>(toLeftOf target: Target,
+                                            offset: CGFloat = 0) -> NSLayoutConstraint
     {
         return constrain(.right, to: .left, of: target, offset: offset)
     }
     
     @discardableResult
-    func constrain<LayoutObject: HasLayoutAnchors>(_ attribute: HorizontalAttribute,
-                                                   to targetAttribute: HorizontalAttribute,
-                                                   of target: LayoutObject,
-                                                   offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainCenterX<Target: LayoutItem>(to target: Target,
+                                              offset: CGFloat = 0) -> NSLayoutConstraint
     {
-        let anchor = attribute == .left ? leftAnchor : rightAnchor
-        let targetAnchor = targetAttribute == .left ? target.leftAnchor : target.rightAnchor
+        return constrain(.centerX, to: .centerX, of: target, offset: offset)
+    }
+    
+    @discardableResult
+    private func constrain<Target: LayoutItem>(_ position: XPosition,
+                                               to targetPosition: XPosition,
+                                               of target: Target,
+                                               offset: CGFloat = 0) -> NSLayoutConstraint
+    {
+        let myAnchor = anchor(for: position)
+        let targetAnchor = target.anchor(for: position)
         
-        let constraint = anchor.constraint(equalTo: targetAnchor, constant: offset)
+        let constraint = myAnchor.constraint(equalTo: targetAnchor, constant: offset)
         
         constraint.isActive = true
         
         return constraint
     }
     
-    // MARK: Vertical
+    private func anchor(for xPosition: XPosition) -> NSLayoutXAxisAnchor
+    {
+        switch xPosition
+        {
+        case .left: return leftAnchor
+        case .centerX: return centerXAnchor
+        case .right: return rightAnchor
+        }
+    }
     
     @discardableResult
-    func constrainTop<LayoutObject: HasLayoutAnchors>(to target: LayoutObject,
-                                                      offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainLeft<Target: LayoutItem>(to relativePosition: CGFloat,
+                                           of target: Target) -> NSLayoutConstraint
+    {
+        let constraint = NSLayoutConstraint(item: self,
+                                            attribute: .left,
+                                            relatedBy: .equal,
+                                            toItem: target,
+                                            attribute: .right,
+                                            multiplier: relativePosition,
+                                            constant: 0)
+        
+        constraint.isActive = true
+        
+        return constraint
+    }
+    
+    // MARK: Vertical Position
+    
+    @discardableResult
+    func constrainTop<Target: LayoutItem>(to target: Target,
+                                          offset: CGFloat = 0) -> NSLayoutConstraint
     {
         return constrain(.top, to: .top, of: target, offset: offset)
     }
     
     @discardableResult
-    func constrainTop<LayoutObject: HasLayoutAnchors>(toBottomOf target: LayoutObject,
-                                                      offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainTop<Target: LayoutItem>(toBottomOf target: Target,
+                                          offset: CGFloat = 0) -> NSLayoutConstraint
     {
         return constrain(.top, to: .bottom, of: target, offset: offset)
     }
     
     @discardableResult
-    func constrainBottom<LayoutObject: HasLayoutAnchors>(to target: LayoutObject,
-                                                         offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainBottom<Target: LayoutItem>(to target: Target,
+                                             offset: CGFloat = 0) -> NSLayoutConstraint
     {
         return constrain(.bottom, to: .bottom, of: target, offset: offset)
     }
     
     @discardableResult
-    func constrainBottom<LayoutObject: HasLayoutAnchors>(toTopOf target: LayoutObject,
-                                                         offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainBottom<Target: LayoutItem>(toTopOf target: Target,
+                                             offset: CGFloat = 0) -> NSLayoutConstraint
     {
         return constrain(.bottom, to: .top, of: target, offset: offset)
     }
     
     @discardableResult
-    func constrain<LayoutObject: HasLayoutAnchors>(_ attribute: VerticalAttribute,
-                                                   to targetAttribute: VerticalAttribute,
-                                                   of target: LayoutObject,
-                                                   offset: CGFloat = 0) -> NSLayoutConstraint
+    func constrainCenterY<Target: LayoutItem>(to target: Target,
+                                              offset: CGFloat = 0) -> NSLayoutConstraint
     {
-        let anchor = attribute == .top ? topAnchor : bottomAnchor
-        let targetAnchor = targetAttribute == .top ? target.topAnchor : target.bottomAnchor
+        return constrain(.centerY, to: .centerY, of: target, offset: offset)
+    }
+    
+    @discardableResult
+    private func constrain<Target: LayoutItem>(_ position: YPosition,
+                                               to targetPosition: YPosition,
+                                               of target: Target,
+                                               offset: CGFloat = 0) -> NSLayoutConstraint
+    {
+        let myAnchor = anchor(for: position)
+        let targetAnchor = target.anchor(for: position)
         
-        let constraint = anchor.constraint(equalTo: targetAnchor, constant: offset)
+        let constraint = myAnchor.constraint(equalTo: targetAnchor, constant: offset)
         
         constraint.isActive = true
         
         return constraint
+    }
+    
+    private func anchor(for yPosition: YPosition) -> NSLayoutYAxisAnchor
+    {
+        switch yPosition
+        {
+        case .top: return topAnchor
+        case .centerY: return centerYAnchor
+        case .bottom: return bottomAnchor
+        }
+    }
+    
+    @discardableResult
+    func constrainTop<Target: LayoutItem>(to relativePosition: CGFloat,
+                                          of target: Target) -> NSLayoutConstraint
+    {
+        let constraint = NSLayoutConstraint(item: self,
+                                            attribute: .top,
+                                            relatedBy: .equal,
+                                            toItem: target,
+                                            attribute: .bottom,
+                                            multiplier: relativePosition,
+                                            constant: 0)
+        
+        constraint.isActive = true
+        
+        return constraint
+    }
+    
+    // MARK: Width
+    
+    @discardableResult
+    func constrainWidth(to width: CGFloat) -> NSLayoutConstraint
+    {
+        return constrain(.width, to: width)
+    }
+    
+    @discardableResult
+    func constrainWidth(toMinimum minimum: CGFloat) -> NSLayoutConstraint
+    {
+        return constrain(.width, toMinimum: minimum)
+    }
+    
+    @discardableResult
+    func constrainWidth<Target: LayoutItem>(to target: Target) -> NSLayoutConstraint
+    {
+        return constrain(.width, to: target)
+    }
+    
+    @discardableResult
+    func constrainWidth<Target: LayoutItem>(to relativeSize: CGFloat,
+                                            of target: Target) -> NSLayoutConstraint
+    {
+        return constrain(.width, to: relativeSize, of: target)
+    }
+    
+    // MARK: Height
+    
+    @discardableResult
+    func constrainHeight(to height: CGFloat) -> NSLayoutConstraint
+    {
+        return constrain(.height, to: height)
+    }
+    
+    @discardableResult
+    func constrainHeight(toMinimum minimum: CGFloat) -> NSLayoutConstraint
+    {
+        return constrain(.height, toMinimum: minimum)
+    }
+    
+    @discardableResult
+    func constrainHeight<Target: LayoutItem>(to target: Target) -> NSLayoutConstraint
+    {
+        return constrain(.height, to: target)
+    }
+    
+    @discardableResult
+    func constrainHeight<Target: LayoutItem>(to relativeSize: CGFloat,
+                                             of target: Target) -> NSLayoutConstraint
+    {
+        return constrain(.height, to: relativeSize, of: target)
     }
     
     // MARK: Size
     
     @discardableResult
-    func constrainWidth(toMinimum minimum: CGFloat) -> NSLayoutConstraint
+    private func constrain(_ dimension: Dimension,
+                           to size: CGFloat) -> NSLayoutConstraint
     {
-        let constraint = widthAnchor.constraint(greaterThanOrEqualToConstant: minimum)
+        let myAnchor = anchor(for: dimension)
+        
+        let constraint = myAnchor.constraint(equalToConstant: size)
         
         constraint.isActive = true
         
@@ -112,9 +245,51 @@ extension HasLayoutAnchors
     }
     
     @discardableResult
-    func constrainWidth<LayoutObject: HasLayoutAnchors>(to target: LayoutObject) -> NSLayoutConstraint
+    private func constrain(_ dimension: Dimension,
+                           toMinimum minimum: CGFloat) -> NSLayoutConstraint
     {
-        let constraint = widthAnchor.constraint(equalTo: target.widthAnchor)
+        let myAnchor = anchor(for: dimension)
+        
+        let constraint = myAnchor.constraint(greaterThanOrEqualToConstant: minimum)
+        
+        constraint.isActive = true
+        
+        return constraint
+    }
+    
+    @discardableResult
+    private func constrain<Target: LayoutItem>(_ dimension: Dimension,
+                                               to target: Target) -> NSLayoutConstraint
+    {
+        let myAnchor = anchor(for: dimension)
+        let targetAnchor = target.anchor(for: dimension)
+        
+        let constraint = myAnchor.constraint(equalTo: targetAnchor)
+        
+        constraint.isActive = true
+        
+        return constraint
+    }
+    
+    private func anchor(for dimension: Dimension) -> NSLayoutDimension
+    {
+        return dimension == .width ? widthAnchor : heightAnchor
+    }
+    
+    @discardableResult
+    private func constrain<Target: LayoutItem>(_ dimension: Dimension,
+                                               to relativeSize: CGFloat,
+                                               of target: Target) -> NSLayoutConstraint
+    {
+        let attribute = dimension.attribute
+        
+        let constraint = NSLayoutConstraint(item: self,
+                                            attribute: attribute,
+                                            relatedBy: .equal,
+                                            toItem: target,
+                                            attribute: attribute,
+                                            multiplier: relativeSize,
+                                            constant: 0)
         
         constraint.isActive = true
         
@@ -124,16 +299,31 @@ extension HasLayoutAnchors
 
 // MARK: -
 
-enum HorizontalAttribute { case left, right }
-enum VerticalAttribute { case top, bottom }
+enum XPosition { case left, centerX, right }
+enum YPosition { case top, centerY, bottom }
+
+enum Dimension
+{
+    case width, height
+    
+    var attribute: NSLayoutConstraint.Attribute
+    {
+        return self == .width ? .width : .height
+    }
+}
 
 // MARK: -
 
-protocol HasLayoutAnchors
+protocol LayoutItem
 {
     var widthAnchor: NSLayoutDimension { get }
-    var topAnchor: NSLayoutYAxisAnchor { get }
-    var bottomAnchor: NSLayoutYAxisAnchor { get }
+    var heightAnchor: NSLayoutDimension { get }
+    
     var leftAnchor: NSLayoutXAxisAnchor { get }
+    var centerXAnchor: NSLayoutXAxisAnchor { get }
     var rightAnchor: NSLayoutXAxisAnchor { get }
+    
+    var topAnchor: NSLayoutYAxisAnchor { get }
+    var centerYAnchor: NSLayoutYAxisAnchor { get }
+    var bottomAnchor: NSLayoutYAxisAnchor { get }
 }
