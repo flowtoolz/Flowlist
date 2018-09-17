a10843d5e1bded9924efceda73216c25fd9c08eb
Sebastian <contact@flowtoolz.com>
2018-09-17 16:56:33 +0200

    provisionally hold NSMenu extension to WindowMenu

--- Code/macOS/WindowMenu.swift
+++ Code/macOS/WindowMenu.swift
@@ -108,3 +108,22 @@ class WindowMenu: NSMenu
     
     private weak var window: Window?
 }
+
+// TODO: replace this an only use MenuItem. be careful: item validation breaks easily...
+public extension NSMenu
+{
+    func item(_ title: String,
+              action: Selector,
+              key: String,
+              modifiers: NSEvent.ModifierFlags = [.command]) -> NSMenuItem
+    {
+        let item = NSMenuItem()
+        item.target = self
+        item.title = title
+        item.action = action
+        item.keyEquivalent = key
+        item.keyEquivalentModifierMask = modifiers
+        
+        return item
+    }
+}
