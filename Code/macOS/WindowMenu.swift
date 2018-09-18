df71a32e29c7751520a2882d851b08e57167aef3
Sebastian <contact@flowtoolz.com>
2018-09-18 12:05:48 +0200

    remove legacy code

--- Code/macOS/WindowMenu.swift
+++ Code/macOS/WindowMenu.swift
@@ -117,22 +117,3 @@ class WindowMenu: NSMenu, Observer
     
     private weak var window: Window?
 }
-
-// TODO: replace this an only use MenuItem. be careful: item validation breaks easily...
-public extension NSMenu
-{
-    func item(_ title: String,
-              action: Selector,
-              key: String,
-              modifiers: NSEvent.ModifierFlags = [.command]) -> NSMenuItem
-    {
-        let item = NSMenuItem()
-        item.target = self
-        item.title = title
-        item.action = action
-        item.keyEquivalent = key
-        item.keyEquivalentModifierMask = modifiers
-        
-        return item
-    }
-}
