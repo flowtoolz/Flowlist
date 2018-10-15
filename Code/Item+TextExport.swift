40a363b439b1c01b4a26d4eed886c2cab0cfcd8c
Sebastian <contact@flowtoolz.com>
2018-10-15 14:53:30 +0200

    implement proper plain text generation

--- Code/Item+TextExport.swift
+++ Code/Item+TextExport.swift
@@ -1,12 +1,31 @@
 extension Tree where Data == ItemData
 {
-    var text: String
+    func text(recursionDepth: Int = 0,
+              titlePrefix: String = "") -> String
     {
         var result = title ?? ""
         
-        for branch in branches
+        if recursionDepth > 0 && count > 0
         {
-            result += "\n\n" + branch.text
+            result = titlePrefix + " " + result
+        }
+        
+        var sectionNumber = 1
+        
+        for item in branches
+        {
+            var itemTitleRefix = titlePrefix
+            if itemTitleRefix.count > 0 { itemTitleRefix += "." }
+            itemTitleRefix += "\(sectionNumber)"
+            
+            let isParagraph = item.isLeaf
+            let breaks: String = "\n\n" + (isParagraph ? "" : "\n")
+            let itemDepth = recursionDepth + 1
+            
+            result += breaks + item.text(recursionDepth: itemDepth,
+                                         titlePrefix: itemTitleRefix)
+            
+            sectionNumber += isParagraph ? 0 : 1
         }
         
         return result
