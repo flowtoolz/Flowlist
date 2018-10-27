533039e8f55803e6e2765729683a8ed19aa75b29
Sebastian <contact@flowtoolz.com>
2018-10-27 12:47:36 +0200

    query iCloud account status, fetch iCloud record

--- Code/macOS/iCloud.swift
+++ Code/macOS/iCloud.swift
@@ -6,19 +6,74 @@ class ICloud
     {
         print("TESTING ICLOUD")
         
-        let container = CKContainer.default()
-        
+        container.accountStatus
+        {
+            status, error in
+            
+            if let error = error
+            {
+                print("An error occured requesting the iCloud account status: \(error.localizedDescription)")
+                return
+            }
+            
+            switch status
+            {
+            case .couldNotDetermine:
+                print("Could not determine iCloud account status.")
+            case .available:
+                print("iCloud account is available.")
+                self.fetchTestRecord()
+//                self.saveTestRecord()
+            case .restricted:
+                print("iCloud account is restricted.")
+            case .noAccount:
+                print("This device is not connected to an iCloud account.")
+            }
+        }
+    }
+    
+    private func fetchTestRecord()
+    {
         let database = container.privateCloudDatabase
         
-        let recordId = CKRecordID(recordName: "test")
+        let recordId = CKRecordID(recordName: "test3")
+        
+        database.fetch(withRecordID: recordId)
+        {
+            fetchedRecord, error in
+            
+            if let error = error
+            {
+                print("An error occured fetching iCloud record: \(error.localizedDescription)")
+                return
+            }
+            
+            print("fetched record: \(fetchedRecord?.debugDescription)")
+            
+            print("all keys: \(fetchedRecord?.allKeys())")
+        }
+    }
+    
+    private func saveTestRecord()
+    {
+        let recordId = CKRecordID(recordName: "test3")
         
         let record = CKRecord(recordType: "Item", recordID: recordId)
         
+        record["text"] = "this is a test text"
+        
+        let database = container.privateCloudDatabase
+        
         database.save(record)
         {
-            (savedRecord, error) in
+            savedRecord, error in
             
-            // handle response
+            print(savedRecord.debugDescription)
         }
+    }
+    
+    private var container: CKContainer
+    {
+        return CKContainer.default()
     }
 }
