7124be6be82d277464b79d0b3b8c1e5f79cdc1b5
Sebastian <contact@flowtoolz.com>
2018-10-30 08:28:14 +0100

    move ItemHash to separate file

---
+++ Code/ItemHash.swift
@@ -0,0 +1,35 @@
+class ItemHash
+{
+    var items: [Item] { return Array(storedItems.values) }
+    
+    subscript(_ id: String) -> Item? { return storedItems[id] }
+    
+    func reset(with items: [Item])
+    {
+        storedItems.removeAll()
+        
+        add(items)
+    }
+    
+    func add(_ items: [Item])
+    {
+        for item in items
+        {
+            guard let data = item.data else { return }
+            
+            storedItems[data.id] = item
+        }
+    }
+    
+    func remove(_ items: [Item])
+    {
+        for item in items
+        {
+            guard let data = item.data else { return }
+            
+            storedItems[data.id] = nil
+        }
+    }
+    
+    private var storedItems = [String : Item]()
+}
