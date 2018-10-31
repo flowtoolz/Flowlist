a710c9cf80ad30175713a636d468ce9710a35705
Sebastian <contact@flowtoolz.com>
2018-10-31 11:19:06 +0100

    add stub: creating item edit events from tree root events

--- Code/ItemEdit.swift
+++ Code/ItemEdit.swift
@@ -1,5 +1,20 @@
 enum ItemEdit
 {
+    // TODO: generalize ItemEdit so it can inform about batch edits
+    init(from event: Item.Event.RootEvent)
+    {
+        switch event
+        {
+        case .didRemove(let items):
+            self = .didNothing
+            
+        case .didInsert(let items,
+                        in: let superItem,
+                        at: let indexes):
+            self = .didNothing
+        }
+    }
+    
     case didNothing
     case didCreate(_ info: ItemEditInfo)
     case didModify(_ info: ItemEditInfo)
