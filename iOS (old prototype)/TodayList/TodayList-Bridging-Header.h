f2b062ac8f358d05263393b83f7cd3d12e8b4707
Sebastian <contact@flowtoolz.com>
2018-11-04 08:50:48 +0100

    remove old iOS prototype and finish up project restructuring

--- iOS (old prototype)/TodayList/TodayList-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  TodayList-Bridging-Header.h
-//  TodayList
-//
-//  Created by Sebastian Fichtner on 08.05.15.
-//  Copyright (c) 2015 Flowtoolz. All rights reserved.
-//
-
-#import "PureLayout.h"
