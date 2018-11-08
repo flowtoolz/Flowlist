7a0a5e529b5501a80a77bc6aae734ca94a41b60b
Sebastian <contact@flowtoolz.com>
2018-11-08 13:10:05 +0100

    set modification positions more efficiently

--- Code/Storage/Edit.swift
+++ Code/Storage/Edit.swift
@@ -5,7 +5,13 @@ enum Edit
         switch treeUpdate
         {
         case .insertedNodes(let nodes, let root, _):
-            let mods = nodes.allItems.compactMap { $0.modification }
+            var mods = [Modification]()
+            
+            for item in nodes
+            {
+                mods.append(contentsOf: item.modifications())
+            }
+            
             self = .insertItem(mods, inItemWithId: root.data.id)
             
         case .receivedDataUpdate(let dataUpdate, let node):
