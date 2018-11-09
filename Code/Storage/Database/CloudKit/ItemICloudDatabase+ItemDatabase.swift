a56d3800c829d43aaa05e1e14d865979018f2fb4
Sebastian <contact@flowtoolz.com>
2018-11-09 13:09:04 +0100

    focus on general edits for now, care about root management later

--- Code/Storage/Database/CloudKit/ItemICloudDatabase+ItemDatabase.swift
+++ Code/Storage/Database/CloudKit/ItemICloudDatabase+ItemDatabase.swift
@@ -21,112 +21,7 @@ extension ItemICloudDatabase: ItemDatabase
         }
     }
     
-    // MARK: - Manage the Root
-    
-    func resetItemTree(with modifications: [Modification])
-    {
-        deleteItems
-        {
-            guard $0 else { return }
-            
-            let records = modifications.map { CKRecord(modification: $0) }
-            
-            self.save(records)
-            {
-                guard $0 else
-                {
-                    // TODO: handle failure
-                    return
-                }
-            }
-        }
-    }
-    
-    func fetchItemTree(receiveRoot: @escaping (Item?) -> Void)
-    {
-        fetchItemRecords()
-        {
-            records in receiveRoot(self.makeItem(from: records))
-        }
-    }
-    
-    private func makeItem(from records: [CKRecord]?) -> Item?
-    {
-        // get record array
-        
-        guard let records = records else
-        {
-            log(warning: "Record array is nil.")
-            
-            return nil
-        }
-        
-        // create unconnected items. remember associated records.
-        
-        var hashMap = [String : (CKRecord, Item)]()
-        
-        for record in records
-        {
-            guard let modification = record.modification else { continue }
-            
-            let id = modification.id
-            
-            hashMap[id] = (record, Item(modification: modification))
-        }
-        
-        // connect items. find root.
-        
-        var root: Item?
-        
-        for (record, item) in hashMap.values
-        {
-            guard let superItemId = record.superItem else
-            {
-                if root != nil
-                {
-                    log(error: "Record array contains more than 1 root.")
-                    
-                    return nil
-                }
-                
-                root = item
-                
-                continue
-            }
-            
-            guard let (_, superItem) = hashMap[superItemId] else
-            {
-                log(error: "Record for super item with id \(superItemId) is missing.")
-                
-                return nil
-            }
-            
-            item.root = superItem
-            
-            superItem.add(item)
-        }
-        
-        if root == nil
-        {
-            log(error: "Record array contains no root.")
-        }
-        
-        // sort and return root
-        
-        root?.sortWithoutSendingUpdate
-        {
-            let leftPosition = hashMap[$0.data.id]?.0.position ?? 0
-            let rightPosition = hashMap[$1.data.id]?.0.position ?? 0
-            
-            return leftPosition < rightPosition
-        }
-        
-        return root
-    }
-    
     // MARK: - Create
-    
-    
     
     func createItems(with modifications: [Modification],
                      inRootWithID rootID: String)
@@ -264,5 +159,108 @@ extension ItemICloudDatabase: ItemDatabase
     {
         deleteRecords(ofType: CKRecord.itemType,
                       handleSuccess: handleSuccess)
+    }
+    
+    // MARK: - Manage the Root
+    
+    func resetItemTree(with modifications: [Modification])
+    {
+        deleteItems
+        {
+            guard $0 else { return }
+            
+            let records = modifications.map { CKRecord(modification: $0) }
+            
+            self.save(records)
+            {
+                guard $0 else
+                {
+                    // TODO: handle failure
+                    return
+                }
+            }
+        }
+    }
+    
+    func fetchItemTree(receiveRoot: @escaping (Item?) -> Void)
+    {
+        fetchItemRecords()
+        {
+            records in receiveRoot(self.makeItem(from: records))
+        }
+    }
+    
+    private func makeItem(from records: [CKRecord]?) -> Item?
+    {
+        // get record array
+        
+        guard let records = records else
+        {
+            log(warning: "Record array is nil.")
+            
+            return nil
+        }
+        
+        // create unconnected items. remember associated records.
+        
+        var hashMap = [String : (CKRecord, Item)]()
+        
+        for record in records
+        {
+            guard let modification = record.modification else { continue }
+            
+            let id = modification.id
+            
+            hashMap[id] = (record, Item(modification: modification))
+        }
+        
+        // connect items. find root.
+        
+        var root: Item?
+        
+        for (record, item) in hashMap.values
+        {
+            guard let superItemId = record.superItem else
+            {
+                if root != nil
+                {
+                    log(error: "Record array contains more than 1 root.")
+                    
+                    return nil
+                }
+                
+                root = item
+                
+                continue
+            }
+            
+            guard let (_, superItem) = hashMap[superItemId] else
+            {
+                log(error: "Record for super item with id \(superItemId) is missing.")
+                
+                return nil
+            }
+            
+            item.root = superItem
+            
+            superItem.add(item)
+        }
+        
+        if root == nil
+        {
+            log(error: "Record array contains no root.")
+        }
+        
+        // sort and return root
+        
+        root?.sortWithoutSendingUpdate
+        {
+            let leftPosition = hashMap[$0.data.id]?.0.position ?? 0
+            let rightPosition = hashMap[$1.data.id]?.0.position ?? 0
+            
+            return leftPosition < rightPosition
+        }
+        
+        return root
     }
 }
--- Code/Storage/Database/ItemDatabase.swift
+++ Code/Storage/Database/ItemDatabase.swift
@@ -3,8 +3,10 @@ import SwiftyToolz
 
 protocol ItemDatabase: Observable where UpdateType == Edit?
 {
+    // general edits
+    func apply(_ edit: Edit)
+    
+    // root management
     func fetchItemTree(receiveRoot: @escaping (Item?) -> Void)
     func resetItemTree(with modifications: [Modification])
-    
-    func apply(_ edit: Edit)
 }
