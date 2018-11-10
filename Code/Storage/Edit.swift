4810b2ad91a32081b801ba1f2c6e51369483f74b
Sebastian <contact@flowtoolz.com>
2018-11-10 15:59:44 +0100

    move to extension: creating Edit from TreeUpdate

--- Code/Storage/Edit.swift
+++ Code/Storage/Edit.swift
@@ -1,37 +1,5 @@
 enum Edit
 {
-    init?(treeUpdate: Item.Event.TreeUpdate)
-    {
-        switch treeUpdate
-        {
-        case .insertedNodes(let nodes, let root, _):
-            var mods = [Modification]()
-            
-            for item in nodes
-            {
-                mods.append(contentsOf: item.modifications())
-            }
-            
-            self = .insertItems(mods, inItemWithId: root.data.id)
-            
-        case .receivedDataUpdate(let dataUpdate, let node):
-            if case .wasModified = dataUpdate
-            {
-                self = .modifyItem(node.modification,
-                                   inItemWithId: node.root?.data.id)
-            }
-            else { return nil }
-            
-        case .removedNodes(let nodes, _):
-            let ids = nodes.compactMap { $0.data.id }
-            self = .removeItemsWithIds(ids)
-            
-        case .movedNode(let node, _, _):
-            self = .modifyItem(node.modification,
-                               inItemWithId: node.root?.data.id)
-        }
-    }
-    
     case insertItems([Modification], inItemWithId: String?)
     case modifyItem(Modification, inItemWithId: String?)
     case removeItemsWithIds([String])
