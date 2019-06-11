e067279aa5de1f44f6ae962179ab5ebe529de799
flowtoolz <contact@flowtoolz.com>
2019-06-11 21:57:40 +0200

    Add TODO

--- Code/Storage/Database/CloudKit/CloudKit-Item Mapping/CKRecord.Reference+ItemZone.swift
+++ Code/Storage/Database/CloudKit/CloudKit-Item Mapping/CKRecord.Reference+ItemZone.swift
@@ -2,6 +2,7 @@ import CloudKit
 
 extension CKRecord.Reference
 {
+    // TODO: start using CKRecord.parent reference property in addition so we can migrate to that property at some point
     convenience init(itemOwnerName: String)
     {
         self.init(recordID: CKRecord.ID(itemID: itemOwnerName),
