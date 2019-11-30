d684350539637f3f23f085078023a7fd8be13237
flowtoolz <contact@flowtoolz.com>
2019-11-30 19:14:25 +0100

    Adapt to change in SwiftObserver

---
+++ Code/macOS/Configuration/ObserverConformances.swift
@@ -0,0 +1,11 @@
+//
+//  ObserverConformances.swift
+//  Flowlist
+//
+//  Created by seb on 30.11.19.
+//  Copyright © 2019 Flowtoolz. All rights reserved.
+//
+
+import SwiftObserver
+
+extension NSObject: Observer {}
